2f48363b7adf1eb8cf883f7eefe374b621ee00a0
Renato Fernandes Soares <renato.f.soares@hotmail.com>
2021-04-05 15:31:01 -0300

    feat: added test project

---
+++ test/Cabother.Swagger.WebApi.Test/Controllers/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Cabother.Swagger.WebApi.Test.Controllers
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
